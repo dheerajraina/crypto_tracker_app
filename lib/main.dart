32ea03c40f73e19674e213bf5c938de18d7a891c
dheerajraina <dheerajraina55@gmail.com>
2021-12-16 14:09:46 +0530

    Replacing vertical list view by horizontal list view

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+// import 'dart:html';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:crypto_tracker/services/api_services.dart';
 import 'dart:core';
@@ -112,7 +115,7 @@ class _HomePageState extends State<HomePage> {
               height: MediaQuery.of(context ).size.width*0.15,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                children: [
+                children:<Widget>[
                 Text(
                   'Crypto Tracker',
                   style: TextStyle(
@@ -124,9 +127,16 @@ class _HomePageState extends State<HomePage> {
                 ],
               ),
             ),
-            Expanded(
+
+            // Padding(padding: EdgeInsets.only(top:MediaQuery.of(context ).size.width*0.3)),
+            
+            
+            Container(
+              margin:EdgeInsets.symmetric(vertical: MediaQuery.of(context ).size.width*0.3),
+              // color: Colors.pink[300],
+              height: MediaQuery.of(context ).size.width*1.0,
               child: ListView.builder(
-                
+                scrollDirection: Axis.horizontal,
                 itemCount: crypto_currencies?.length,
                 
                 itemBuilder: (context , index){
@@ -136,32 +146,96 @@ class _HomePageState extends State<HomePage> {
                 // }
                 
                   return Card(
-                    
-                    child: ListTile(
-                      leading: CircleAvatar(
-                        backgroundColor: Color((math.Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0),
+                    shadowColor: Colors.black,
+                    child: Container(
+                      
+                      // color: Colors.green,
+                      width: MediaQuery.of(context ).size.width*0.7,
+                      decoration: BoxDecoration(
+                        color: Color((math.Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0),
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [BoxShadow(
+                          // offset: Offset.zero,
+                          color: Colors.black,
+                          blurRadius: 40.0,
+                        ),]
+                        // border: Border.all()
                         
                       ),
-                      title: Text(
-                        crypto_currencies?[index]['name'],
-                        style: TextStyle(
-                          fontSize: MediaQuery.of(context).size.width*0.05,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        ),
-                    subtitle: Text (
-                      ((crypto_currencies?[index]['price_usd']).toString()),
-                      style: TextStyle(
-                        color: Colors.red,
+                      child: Column(
+                        children: [
+                          SizedBox(
+                            // width:MediaQuery.of(context ).size.width*0.01 ,
+                            height:MediaQuery.of(context ).size.width*0.07,
+                            
+                          ),
+                          Text(
+                            ((crypto_currencies?[index]['asset_id']).toString()),
+
+                            style: TextStyle(
+                              fontSize: MediaQuery.of(context ).size.width*0.05,
+                              fontWeight: FontWeight.bold,
+                            
+                            ),
+                          ),
+                          Text(
+                            crypto_currencies?[index]['name'],
+
+                            style: TextStyle(
+                              fontSize: MediaQuery.of(context ).size.width*0.08,
+                              fontWeight: FontWeight.bold,
+                              
+                            ),
+
+                          ),
+                          SizedBox(
+                            // width:MediaQuery.of(context ).size.width*0.01 ,
+                            height:MediaQuery.of(context ).size.width*0.07,
+                            
+                          ),
+                          Text(
+                            'Current Price',
+                            style:TextStyle(
+                              fontSize: MediaQuery.of(context ).size.width*0.05,
+                            ),
+                          ),
+                          Text(
+                            ((crypto_currencies?[index]['price_usd']).toString()),
+
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: MediaQuery.of(context ).size.width*0.05,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+
+                          SizedBox(
+                            // width:MediaQuery.of(context ).size.width*0.01 ,
+                            height:MediaQuery.of(context ).size.width*0.07,
+                            
+                          ),
+
+                          Text(
+                            'Total Volume',
+                            style:TextStyle(
+                              fontSize: MediaQuery.of(context ).size.width*0.05,
+                            ),
+                          ),
+                          Text(
+                            ((crypto_currencies?[index]['volume_1mth_usd']).toString()),
+                            style:TextStyle(
+                              fontSize: MediaQuery.of(context ).size.width*0.05,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
                       ),
-                      ), 
-                          
-                         
-                        
+                      // height: MediaQuery.of(context ).size.height*0.1,
                     ),
                     
+                    
                   );
-            
+              
                 },
                 ),
             ),
